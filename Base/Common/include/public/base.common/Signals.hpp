b1b35d8e636b280d20d29173e4755c111e0223d6
Phlar <>
2018-07-09 21:50:38 +0200

    Added first draft of 'modules'-target. Build broken.

---
+++ Base/Common/include/public/base.common/Signals.hpp
@@ -0,0 +1,19 @@
+#ifndef AWSOFTWARE_BASE_COMMON_IMODULE_HPP
+#define AWSOFTWARE_BASE_COMMON_IMODULE_HPP
+
+namespace aw {
+namespace base {
+namespace common {
+
+template<typename... Args>
+struct Signal {
+};
+
+struct SignalConnection {
+};
+
+} // namespace common
+} // namespace base	
+} // namespace aw
+
+#endif // AWSOFTWARE_BASE_COMMON_IMODULE_HPP
