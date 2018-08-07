583607903cbe20612c74a52da85752768954c976
Phlar <>
2018-08-07 23:52:18 +0200

    Added 'libCommon' dependency to 'libSceneGraph'. Fixed compilation issues of TypedNodeOccurrence.

--- SceneGraph/Base/include/internal/scenegraph.base.internal/TypedNodeOccurrence.hpp
+++ SceneGraph/Base/include/internal/scenegraph.base.internal/TypedNodeOccurrence.hpp
@@ -3,6 +3,9 @@
 
 #include <memory>
 
+#include "base.common/Assert.hpp"
+
+#include "scenegraph.base.internal/INodeVisitor.hpp"
 #include "scenegraph.base.internal/NodeOccurrence.hpp"
 
 
@@ -11,38 +14,40 @@ namespace scenegraph {
 namespace base {
 namespace internal {
 
-//template<typename T>
-//class TypedNodeOccurrence : public NodeOccurrence,
-//                            public std::enable_shared_from_this<TypedNodeOccurrence<T>> {
-//    public:
-//
-//        // Allow friendship to arbitrary typed NodeInstances.
-//        template<typename U>
-//        friend class NodeInstance<U>;
-//
-//        using TypedNodeInstancePtr = std::shared_ptr<NodeInstance<typename T::TInstance>>;
-//
-//        TypedNodeOccurrence() = delete;
-//        TypedNodeOccurrence(TypedNodeInstancePtr instance)
-//        : m_instance(instance) {
-//            // Todo: Assert the instance.
-//        }
-//
-//        virtual ~TypedNodeOccurrence() {
-//        }
-//
-//        NodeInstancePtr getInstance() const {
-//            return m_instance;
-//        }
-//
-//        void accept(detail::NodeVisitorPtr& visitor) final {
-//        }
-//
-//    protected:
-//
-//        NodeInstanceWeakPtr m_parent;
-//        TypedNodeInstancePtr m_instance;
-//};
+template<typename T>
+class TypedNodeOccurrence : public NodeOccurrence,
+                            public std::enable_shared_from_this<TypedNodeOccurrence<T>> {
+    public:
+
+        // Allow friendship to arbitrary typed NodeInstances.
+        template<typename U>
+        friend class TypedNodeInstance;
+
+        using TypedNodeInstancePtr = std::shared_ptr<TypedNodeInstance<typename T::TInstance>>;
+
+        TypedNodeOccurrence() = delete;
+        TypedNodeOccurrence(TypedNodeInstancePtr instance)
+        : m_instance(instance) {
+            ASSERT(m_instance, "Error setting up node-occurrence, invalid node-instance provided.");
+        }
+
+        virtual ~TypedNodeOccurrence() {
+        }
+
+        NodeInstancePtr getInstance() const {
+            return m_instance;
+        }
+
+        void accept(INodeVisitorPtr& visitor) final {
+            // Todo: Assert the visitor.
+            visitor->visit(shared_from_this());
+        }
+
+    protected:
+
+        NodeInstanceWeakPtr m_parent;
+        TypedNodeInstancePtr m_instance;
+};
 
 } // namespace internal
 } // namespace base
--- SceneGraph/Base/testing/TestTypedNodeOccurrence.cpp
+++ SceneGraph/Base/testing/TestTypedNodeOccurrence.cpp
@@ -6,6 +6,25 @@
 #include "scenegraph.base.internal/TypedNodeOccurrence.hpp"
 
 
+using namespace aw::scenegraph::base::internal;
+
+struct TestInstance {
+};
+
+struct TestOccurrence {
+};
+
+struct TestTrait {
+    using TInstance = TestInstance;
+    using TOccurrence = TestOccurrence;
+};
+
+
+TEST(TestTypedNodeOccurrence, TestConstructionInvalidParameters) {
+
+    EXPECT_ANY_THROW(TypedNodeOccurrence<TestTrait>(nullptr));
+}
+
 
 int main(int argc, char **argv) {
      testing::InitGoogleTest(&argc, argv);
